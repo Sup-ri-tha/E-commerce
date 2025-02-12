7b1dec59533cb5339b797947719408c0e2eb82bc
Sup-ri-tha <supritharajashekar10@gmail.com>
2025-02-12 10:15:02 +0530

    made changes in productCard

--- Backend/src/Controllers/Product.js
+++ Backend/src/Controllers/Product.js
@@ -1,39 +1,103 @@
-const {Router} = require('express');
-const { productupload } = require('../Middleware/multer');
-const ProductModel = require('../Model/ProductModel');
-const productrouter = Router();
+// const {Router} = require('express');
+// const { productupload } = require('../Middleware/multer');
+// const ProductModel = require('../Model/ProductModel');
+// const productrouter = Router();
 
-productrouter.get("/", (req, res) => {
-    res.send("Product router");
+// productrouter.get("/", (req, res) => {
+//     res.send("Product router");
+// })
+
+// productrouter.post("/",productupload.array('files') ,async(req, res) => {
+//     const {name, description, price, stock, email, category, tag} = req.body;
+//     const images = req.files.map((file) => file.path);
+//     try{
+//         const seller = await ProductModel.findOne({email:email});
+//         if(!seller){
+//             return res.status(400).json({message:"Seller not found"});
+//         }
+
+//         if(images.length===0){
+//             return res.status(400).json({message:"Please upload one image"});
+//         }
+
+//         await ProductModel.create({
+//             name:name,
+//             description:description,
+//             price:price,
+//             stock:stock,
+//             email:email,
+//             img:images,
+//             category:category,
+//             tag:tag
+//         })
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// })
+
+// module.exports = productrouter;
+
+const {Router}= require('express');
+const { productupload } = require('../../multer');
+const Productmodel = require('../Model/Productmodel');
+const productrouter=Router();
+
+
+productrouter.get("/get-router",async (req,res)=>{      
+    try{
+        const productfind = await Productmodel.find();
+        console.log(productfind);
+        const productimage = productfind.map((product)=>{{
+     return{
+            name:product.name,
+            description:product.description,
+            category:product.category,
+            tags:product.tags,
+            price:product.price,
+            stock:product.stock,
+            email:product.email,
+            images:product.images
+        }       
+    }})
+    res.status(200).json({products:productimage});
+    }
+    catch(error){
+        console.error(error);
+    }
 })
 
-productrouter.post("/",productupload.array('files') ,async(req, res) => {
-    const {name, description, price, stock, email, category, tag} = req.body;
-    const images = req.files.map((file) => file.path);
-    try{
-        const seller = await ProductModel.findOne({email:email});
+productrouter.post("/post-product",productupload.array('files'),async (req,res)=>{
+    const {name, description, category,tags,price,stock,email}=req.body;
+     const images = req.files.map(file => file.path);
+     try{
+        const seller=await Productmodel.findOne({email:email}); 
         if(!seller){
             return res.status(400).json({message:"Seller not found"});
+        }     
+
+        if(images.length===0){
+            return res.status(400).json({message:"Please upload atleast one image"});
         }
 
-        // if(images.length===0){
-        //     return res.status(400).json({message:"Please upload one image"});
-        // }
-
-        await ProductModel.create({
+       const newproduct= await Productmodel.create({
             name:name,
             description:description,
+            category:category,
+            tags:tags,
             price:price,
             stock:stock,
             email:email,
-            img:images,
-            category:category,
-            tag:tag
+            images:images
         })
-    }
-    catch(error){
-        console.log(error);
-    }
+
+        res.status(200).json({message:"Product created successfully",product:newproduct});
+
+     }
+     catch(error){
+         console.error(error);
+     }  
 })
 
-module.exports = productrouter;
+
+module.exports=productrouter;
